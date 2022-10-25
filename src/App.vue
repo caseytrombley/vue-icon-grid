e13ca09ecf65d9457028a23e61cd1f5068745ad0
Casey Trombley <casey@algaecal.com>
2022-10-24 22:11:58 -0400

    feature: project scaffolding and setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <icon-grid />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import IconGrid from "@/components/IconGrid";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {IconGrid},
 }
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap');
+
+html {
+  box-sizing: border-box;
+}
+
+body {
+  background: linear-gradient(132deg, rgb(65, 80, 95) 0.00%, rgb(36, 37, 38) 100.00%);
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px 20px;
+  height: 100vh;
 }
 </style>
---
+++ src/components/IconGrid.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="row">
+    <draggable
+        :list="list"
+        :options="{delay:1000}"
+        class="list-group"
+        ghost-class="ghost"
+    >
+      <div
+          class="list-group-item"
+          v-for="element in list"
+          :key="element.label"
+      >
+        <div class="item-wrapper">
+          <picture>
+            <source
+                :srcset="require(`/src/assets/${element.title}.png`)"
+                media="(min-width: 768px)"
+            >
+            <img
+                :src="require(`/src/assets/small/${element.title}.png`)"
+                alt="Album cover"
+                class="image"
+            >
+          </picture>
+          <span class="label">
+            {{ element.label }}
+          </span>
+        </div>
+      </div>
+    </draggable>
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable';
+
+export default {
+  name: "IconGrid",
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      list: [
+        { id: 0, label: "One", title: 'with-the-beatles' },
+        { id: 1, label: "Two", title: 'a-hard-days-night' },
+        { id: 2, label: "Three", title: 'beatles-for-sale' },
+        { id: 3, label: "Four", title: 'help' },
+        { id: 4, label: "Five", title: 'rubber-soul' },
+        { id: 5, label: "Six", title: 'revolver' },
+        { id: 5, label: "Seven", title: 'magical-mystery-tour' },
+        { id: 5, label: "Eight", title: 'sgt-peppers' },
+        { id: 5, label: "Nine", title: 'white-album' },
+        { id: 5, label: "Ten", title: 'yellow-submarine' },
+        { id: 5, label: "Eleven", title: 'abbey-road' },
+        { id: 5, label: "Twelve", title: 'let-it-be' },
+      ],
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.list-group {
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+
+  @media (min-width: 500px) {
+    grid-template-columns: repeat(4, auto);
+  }
+
+  @media (min-width: 1000px) {
+    grid-template-columns: repeat(6, auto);
+  }
+}
+
+.list-group-item {
+  margin: 0 auto;
+  padding: 10px;
+
+  .item-wrapper {
+    width: 80px;
+    color: white;
+    cursor: move;
+
+    .image {
+      width: 100%;
+      height: 80px;
+      border-radius: 8px;
+      box-shadow: rgba(0, 0, 0, 0.40) 0 1px 4px 0, rgba(99, 99, 99, 0.2) 0 2px 8px 0;
+    }
+
+    .label {
+      font-size: .75em;
+    }
+  }
+
+  &.sortable-chosen {
+    .image {
+      opacity: .8;
+      background: white;
+      animation: shake 150ms infinite;
+    }
+  }
+
+  &.ghost {
+    .image {
+      opacity: .4;
+      background: #cccccc;
+      animation: none;
+    }
+  }
+
+}
+
+@keyframes shake {
+  0% { transform: rotate(0deg); }
+  25% { transform: rotate(3deg); }
+  50% { transform: rotate(0deg); }
+  75% { transform: rotate(-3deg); }
+  100% { transform: rotate(0deg); }
+}
+
+</style>
